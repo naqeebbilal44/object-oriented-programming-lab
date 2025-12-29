c96913cd12a0836eca012d4d9a9eb41f8a17d2a4
naqeebbilal44 <fayzanbhat4498@gmail.com>
2025-12-29 19:15:10 +0530

    Add files via upload

---
+++ chapter9/problem_7.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+class Counter
+{
+protected:
+    unsigned int count;
+
+public:
+    Counter() : count() {}
+
+    explicit Counter(unsigned int c) : count(c) {}
+
+    Counter operator++() { return Counter(++count); }
+
+    Counter operator--() { return Counter(--count); }
+
+    unsigned int getCount() const { return count; }
+};
+
+class Count : public Counter
+{
+public:
+    Count() : Counter() {}
+
+    explicit Count(unsigned int c) : Counter(c) {}
+
+    Count operator++(int)
+    {
+        count++;
+        return Count(count - 1);
+    }
+
+    Count operator--(int)
+    {
+        count--;
+        return Count(count + 1);
+    }
+};
+
+int main()
+{
+    Count c1;
+    c1++;
+    c1--;
+    return 0;
+}
